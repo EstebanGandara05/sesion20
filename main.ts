16ca4dbc2501cbc2d7c1dcec2d13620bdb5b1aec
EstebanGandara05 <112582675+EstebanGandara05@users.noreply.github.com>
2022-10-12 12:40:41 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let x = 0
+let y = 0
+let speed = 200
+basic.forever(function () {
+    for (let index = 0; index < 3; index++) {
+        for (let x = 0; x <= 4; x++) {
+            for (let y = 0; y <= 4; y++) {
+                led.plot(x, y)
+                basic.pause(speed)
+                basic.clearScreen()
+            }
+        }
+        speed += -80
+    }
+    speed = 200
+})
